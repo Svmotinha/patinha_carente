b760a475ad32d4a908c049e99849ecf7b8491885
Svmotinha <rodrigues.samuelvr@gmail.com>
2026-06-14 17:43:28 -0500

    estabelencendo arquitetura do projeto

---
+++ carente/src/test/java/com/carente/patinha/PatinhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carente.patinha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatinhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
